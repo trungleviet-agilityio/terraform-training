45b9516d96135af4b0ac8a7d50ca6c6ee0459510
trungle-agilityio <trung.leviet@asnet.com.vn>
2025-10-28 14:44:58 +0700

    feat(module-04): setting up roles (refs #11)

---
+++ exercises/advanced/module-04/solution/roles.tf
@@ -0,0 +1,27 @@
+locals {
+  role_policies = {
+    readonly = [
+      "ReadOnlyAccess"
+    ]
+    admin = [
+      "AdministratorAccess"
+    ]
+    auditor = [
+      "SecurityAudit"
+    ]
+    developer = [
+      "AmazonVPCFullAccess",
+      "AmazonEC2FullAccess",
+      "AmazonRDSFullAccess"
+    ]
+  }
+
+  role_policies_list = flatten([
+    for role, policies in local.role_policies : [
+      for policy in policies : {
+        role   = role
+        policy = policy
+      }
+    ]
+  ])
+}
