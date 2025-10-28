c1fd54922bff9e2759381a9c4e573b6399d29a04
trungle-agilityio <trung.leviet@asnet.com.vn>
2025-10-28 10:01:48 +0700

    feat(module-03): allow nginx image (refs #10)

--- exercises/advanced/module-03/solution/compute.tf
+++ exercises/advanced/module-03/solution/compute.tf
@@ -1,6 +1,7 @@
 locals {
   ami_ids = {
     ubuntu = data.aws_ami.ubuntu.id
+    nginx  = data.aws_ami.nginx.id
   }
 }
 
@@ -19,11 +20,27 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+data "aws_ami" "nginx" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    # values = ["bitnami-nginx-1.25.4-*-linux-debian-12-x86_64-hvm-ebs-*"]
+    values = ["bitnami-nginx-1.28.0-*-debian-12-amd64-*"]
+    # bitnami-nginx-1.28.0-r11-debian-12-amd64-f5774628-e459-457a-b058-3b513caefdee
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "from_list" {
   count         = length(var.ec2_instance_config_list)
   ami           = local.ami_ids[var.ec2_instance_config_list[count.index].ami]
   instance_type = var.ec2_instance_config_list[count.index].instance_type
-  subnet_id     = aws_subnet.main[
+  subnet_id = aws_subnet.main[
     count.index % length(aws_subnet.main)
   ].id
 
