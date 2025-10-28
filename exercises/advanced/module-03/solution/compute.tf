0dd76954346f64e803f5137db255c6e3ef6d7eb4
trungle-agilityio <trung.leviet@asnet.com.vn>
2025-10-28 09:44:29 +0700

    feat(module-03): multiple ec2 list input (refs #10)

--- exercises/advanced/module-03/solution/compute.tf
+++ exercises/advanced/module-03/solution/compute.tf
@@ -1,3 +1,9 @@
+locals {
+  ami_ids = {
+    ubuntu = data.aws_ami.ubuntu.id
+  }
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Owner is Canonical
@@ -13,10 +19,10 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-resource "aws_instance" "from_count" {
-  count         = var.ec2_instance_count
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+resource "aws_instance" "from_list" {
+  count         = length(var.ec2_instance_config_list)
+  ami           = local.ami_ids[var.ec2_instance_config_list[count.index].ami]
+  instance_type = var.ec2_instance_config_list[count.index].instance_type
   subnet_id     = aws_subnet.main[
     count.index % length(aws_subnet.main)
   ].id
--- exercises/advanced/module-03/solution/variables.tf
+++ exercises/advanced/module-03/solution/variables.tf
@@ -7,3 +7,11 @@ variable "ec2_instance_count" {
   type    = number
   default = 1
 }
+
+variable "ec2_instance_config_list" {
+  type = list(object({
+    instance_type = string
+    ami           = string
+  }))
+  default = []
+}
