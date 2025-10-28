fcd969eebb06a164c6a107820f276a577f9cf2f8
trungle-agilityio <trung.leviet@asnet.com.vn>
2025-10-28 09:18:41 +0700

    feat(module-03): referencing count (refs #10)

---
+++ exercises/advanced/module-03/solution/networking.tf
@@ -0,0 +1,33 @@
+locals {
+  project = "11-multiple-resources"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Project = local.project
+    Name    = local.project
+  }
+}
+
+resource "aws_subnet" "main" {
+  count      = var.subnet_count
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.${count.index}.0/24"
+
+  tags = {
+    Project = local.project
+    Name    = "${local.project}-${count.index}"
+  }
+}
+
+# resource "aws_subnet" "main" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = "10.0.0.0/24"
+
+#   tags = {
+#     Project = local.project
+#     Name    = "${local.project}-${count.index}"
+#   }
+# }
