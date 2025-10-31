f03be78b57e7a34b58afe486b727d1b91222c728
trungle-agilityio <trung.leviet@asnet.com.vn>
2025-10-31 15:36:09 +0700

    feat(main): get the aws account id, region for 20_ 30_ layers (refs #36)

--- practice/deploy/30_app/main/main.tf
+++ practice/deploy/30_app/main/main.tf
@@ -1,1 +1,6 @@
+# Get the AWS account ID and region
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
 # App workloads (API Lambda, SQS worker, cron producer) will be implemented in a later task.
