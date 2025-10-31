c9d5de487bac133cf46a127586941332bbfed5cc
trungle-agilityio <trung.leviet@asnet.com.vn>
2025-10-31 15:31:51 +0700

    feat(providers): add s3 backend configfuration, default_tags for aws provider (refs #36)

--- practice/deploy/20_infra/environments/dev/providers.tf
+++ practice/deploy/20_infra/environments/dev/providers.tf
@@ -1,13 +1,35 @@
 terraform {
   required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
+
+  # S3 backend configuration
+  backend "s3" {
+    bucket         = "tt-practice-tf-state-<unique>"
+    key            = "infra/terraform.tfstate"
+    region         = "ap-southeast-1" # Singapore
+    encrypt        = true
+    # dynamodb_table = "tt-practice-tf-locks" # Consider using this for locking the state file
+  }
 }
 
+# Configure the AWS provider
 provider "aws" {
   region = var.aws_region
+
+  default_tags {
+    tags = merge(
+      {
+        Environment = var.environment
+        Project     = var.project_name
+        ManagedBy   = "Terraform"
+      },
+      var.tags
+    )
+  }
 }
