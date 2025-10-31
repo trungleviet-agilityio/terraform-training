544f1f3195f0d0d94b25dee7d71eef03ae2063df
trungle-agilityio <trung.leviet@asnet.com.vn>
2025-10-31 17:31:00 +0700

    feat(10_core): add s3 modules (refs #47)

--- practice/deploy/10_core/environments/dev/providers.tf
+++ practice/deploy/10_core/environments/dev/providers.tf
@@ -9,12 +9,14 @@ terraform {
   }
 
   # S3 backend configuration
+  # NOTE: Update bucket name after state backend is created (see bootstrap-state.md)
+  # Temporarily disabled to bootstrap state backend - uncomment after first apply
   backend "s3" {
-    bucket         = "tt-practice-tf-state-<unique>"
+    bucket         = "tt-practice-tf-state-dev-057336397237"
     key            = "core/terraform.tfstate"
     region         = "ap-southeast-1" # Singapore
     encrypt        = true
-    # dynamodb_table = "tt-practice-tf-locks" # Consider using this for locking the state file
+    dynamodb_table = "tt-practice-tf-locks"
   }
 }
 
